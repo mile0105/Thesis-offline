a63408117b9fdde1a5347d3eec564e111a574bd7
Mile <mile@producement.com>
2021-05-20 13:03:56 +0200

    Init offline

---
+++ floodfill.js
@@ -0,0 +1,59 @@
+//import {matrixDistance} from "./helper";
+
+
+const decreasingFactor = 0.1;
+const heightAllowanceFactor = 5;
+const distancePerStation = 500;
+
+//optimize later
+const floodFill = (points, station, stationI, stationJ, max) => {
+
+  const traversed = new Set();
+  const fillQueue = [];
+
+  const stationHeight = points[stationI][stationJ].z;
+  const stationPm10Value = station.pm10Value;
+
+  fillQueue.push([stationI, stationJ, stationPm10Value]);
+
+  while (fillQueue.length > 0) {
+
+    const [i, j, pm10] = fillQueue.shift();
+
+    if (i < 0 || j < 0 || i > points.length - 1 || j > points[0].length - 1 || (i-stationI + j-stationJ) > distancePerStation) {
+      continue; //we reached a wall
+    }
+
+    if (traversed.has(`[${i}][${j}]`)) {
+      continue;
+    }
+
+    const x = points[i][j].x;
+    const y = points[i][j].y;
+    const z = points[i][j].z;
+
+    let pm10Value = stationHeight + heightAllowanceFactor >= z && pm10 > 0 ? pm10: 0;
+
+    pm10Value = getBigger(points[i][j].pm10, pm10Value);
+    points[i][j] = {
+      x: x,
+      y: y,
+      z: z,
+      pm10: pm10Value,
+    };
+
+    traversed.add(`[${i}][${j}]`);
+
+    fillQueue.push([i+1, j, pm10Value - decreasingFactor]);
+    fillQueue.push([i-1, j, pm10Value - decreasingFactor]);
+    fillQueue.push([i, j+1, pm10Value - decreasingFactor]);
+    fillQueue.push([i, j-1, pm10Value - decreasingFactor]);
+
+  }
+};
+
+const getBigger = (a, b) => {
+  return a > b ? a : b;
+};
+
+
---
+++ index.js
@@ -0,0 +1,127 @@
+/*import 'ol/ol.css';
+import {Map, View, Feature} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import Point from 'ol/geom/Point';
+import {fromLonLat} from "ol/proj";
+import {Vector} from "ol/source";
+import WebGLPointsLayer from "ol/layer/WebGLPoints";
+import {readPoints, readStations} from "./readService";
+import {floodFill} from "./floodfill";
+import {getMaxPollutionFromStations, getNearestPointPosition} from "./helper";
+*/
+
+const init = () => {
+  const points = readPoints();
+  const stations = readStations();
+  let interpolatedPoints = [];
+
+  let max = getMaxPollutionFromStations(stations);
+
+  for (let station of stations) {
+    const nearestPointPositions = getNearestPointPosition(points, station);
+
+    const i = nearestPointPositions[0];
+    const j = nearestPointPositions[1];
+
+    floodFill(points, station, i, j, max);
+  }
+
+  for (let point of points.flat()) {
+    interpolatedPoints.push({
+      x: point.x,
+      y: point.y,
+      pm10: point.pm10 * 255 / max,
+    })
+  }
+
+  let features = [];
+  const N = interpolatedPoints.length;
+
+  const centerPoint = interpolatedPoints[N - 1];
+
+  for (let point of interpolatedPoints) {
+    const pm10 = point.pm10;
+
+    const feature = new ol.Feature({
+      geometry: new ol.geom.Point(
+        ol.proj.fromLonLat([point.x, point.y])
+      ),
+      // color: pm10,
+    });
+
+    feature.setStyle(new ol.style.Style({
+      image: new ol.style.Circle({
+        fill: new ol.style.Fill({
+          color: [pm10, pm10, pm10, 0.6],//`rgba(${pm10},${pm10},${pm10},0.6)`,
+        })
+      })}));
+    features.push(feature);
+  }
+
+  console.log(features);
+
+  // const layerStyle = new ol.style.Style({
+  //
+  //   fill: new Fill({
+  //     color: 'rgba(255, 255, 255, 0.6)',
+  //   }),
+    /*
+    symbol: {
+      symbolType: 'square',
+      size: [
+        'interpolate',
+        ['exponential', 4],
+        ['zoom'],
+        2, 1,
+        15, 60
+      ],
+      color: ['interpolate',
+        [
+          'exponential',
+          0.5
+        ],
+        [
+          'get',
+          'color'
+        ],
+        0,
+        "#000000",
+        255,
+        "#ffffff"
+      ],
+      rotateWithView: false,
+      offset: [0, 0],
+      opacity: 80
+    }*/
+  // });
+
+  const vector = new ol.source.Vector({features: []});
+
+  vector.addFeatures(features);
+
+  const vectorLayer = new ol.layer.Vector({ //WebGLPoints.WebGLPointsLayer({
+    source: vector,
+    // style: layerStyle,
+  });
+
+  const map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      }),
+      vectorLayer,
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([centerPoint.x, centerPoint.y]),
+      zoom: 10,
+      maxZoom: 13,
+    })
+  });
+  map.render();
+
+};
+
+init();
+
